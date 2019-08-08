ddc6a85849336b978c7c43cc09cc975fc818ecb0
xuyao <xy9123@126.com>
2019-08-08 12:33:41 +0800

    fix

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var Wallet = require('ethereumjs-wallet');
 var ProviderEngine = require("web3-provider-engine");
 var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
 var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+//var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+//var RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
+
 var Web3 = require("web3");
 
 function WalletProvider(privateKey,provider_url) {
@@ -15,7 +17,8 @@ function WalletProvider(privateKey,provider_url) {
   this.engine = new ProviderEngine();
   this.engine.addProvider(new WalletSubprovider(this.wallet, {}));
   this.engine.addProvider(new FiltersSubprovider());
-  this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
+  //this.engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));
+  
   this.engine.start(); // Required by the provider engine.
 };
 
